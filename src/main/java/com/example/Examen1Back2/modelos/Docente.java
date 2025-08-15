c72868b2661e46a7471f086ec6c7b11044ac0963
Yeison <pepsimango234@gmail.com>
2025-08-14 21:22:30 -0500

    fix: Modelo Docente corregido: Syntax - Constructores - Anotaciones

--- src/main/java/com/example/Examen1Back2/modelos/Docente.java
+++ src/main/java/com/example/Examen1Back2/modelos/Docente.java
@@ -1,13 +1,14 @@
 package com.example.Examen1Back2.modelos;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-
+// Correccion aplicada, el jakarta tenia un * e iba id para que @Id funcionara
 import java.util.List;
 
-@Entit
+@Entity
+@Table(name = "docentes")
 public class Docente {
 
-
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
@@ -24,20 +25,43 @@ public class Docente {
     private Usuario usuario;
 
 
+    public Docente() {
+    }
+
+    public Docente(Integer id, String especialidad, List<Curso> cursos, Usuario usuario) {
+
+        this.especialidad = especialidad;
+        this.cursos = cursos;
+        this.usuario = usuario;
+    }
+
 
     public Integer getId() {
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public String getEspecialidad() {
         return especialidad;
     }
 
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+
     public void setEspecialidad(String especialidad) {
         this.especialidad = especialidad;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 }
